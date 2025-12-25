e870cc193b1af71b0bd94dadf464141b829c7222
Shadow05 <nightshadow3243@gmail.com>
2025-12-25 14:58:10 +0200

    Second commit

---
+++ TerminalCalculator.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+std::cout << "************CALCULATOR************ \n";
+
+    std::cout << "Enter either (+ - * / p) : ";
+    std::cin >> op;
+
+    std::cout << "Enter number 1:";
+    std::cin >> num1;
+
+    std::cout << "Enter number 2:";
+    std::cin >> num2;
+
+    switch(op){
+    case '+':
+         result = num1 + num2;
+        std::cout << "RESULT:" << result << '\n';
+        break;
+
+    case '-':
+         result = num1 - num2;
+        std::cout << "RESULT:" << result << '\n';
+        break;
+    case '*':
+         result = num1 * num2;
+        std::cout << "RESULT:" << result << '\n';
+        break;
+    case '/':
+         result = num1 / num2;
+        std::cout << "RESULT:" << result << '\n';
+        break;
+    case 'p':
+         result = pow(num1, num2);
+         std::cout <<"RESULT:" << result << '\n';
+         break;
+
+    default:
+            std::cout << "invalid response";
+            break;
+
+    return 0;
+    }
+
+std::cout << "************************************";
+
+}
